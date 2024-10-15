b503d8948401681e48a608fb4dda38d8c7443494
Moran Talmon <ideal.dusk9861@fastmail.com>
2024-10-15 09:55:20 +0200

    Add EF Core and configure SQLite database
    Configured Entity Framework Core with SQLite for data persistence by adding necessary package references and setting up the DbContext in the program initialization. This change enables the application to interact with a SQLite database for storing and retrieving data.

--- BuberBreakfast/Program.cs
+++ BuberBreakfast/Program.cs
@@ -1,11 +1,16 @@
+using BuberBreakfast.Persistence;
 using BuberBreakfast.Services.Breakfasts;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services.AddControllers();
     builder.Services.AddScoped<IBreakfastService, BreakfastService>();
+    builder.Services.AddDbContext<BuberBreakfastDbContext>(options =>
+    {
+        options.UseSqlite("Data source = BuberBreakfast.db");
+    });
 }
-
 var app = builder.Build();
 {
     app.UseExceptionHandler("/error");
