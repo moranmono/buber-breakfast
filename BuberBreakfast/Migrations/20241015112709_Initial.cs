08b1e7b5f5e78eee943d93e0e0875277312bbb29
Moran Talmon <ideal.dusk9861@fastmail.com>
2024-10-15 13:46:09 +0200

    Add initial database migration
    Create the initial database schema for the BuberBreakfast context. This includes a Breakfasts table with properties such as Id, Name, Description, StartDateTime, EndDateTime, LastModifiedDateTime, Savory, and Sweet.

---
+++ BuberBreakfast/Migrations/20241015112709_Initial.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BuberBreakfast.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Breakfasts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "TEXT", maxLength: 150, nullable: false),
+                    StartDateTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    EndDateTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    LastModifiedDateTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Savory = table.Column<string>(type: "TEXT", nullable: false),
+                    Sweet = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Breakfasts", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Breakfasts");
+        }
+    }
+}
